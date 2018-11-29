c1e4a01c56d0a6ee48370b9ddc2f3dcab2ea96db
Zach Dunbrack <zdunbrack@users.noreply.github.com>
2018-11-28 23:58:46 -0500

    Fixed bug with update() function in TileRow causing double-updates on forward transitions.

--- src/towerdefense/game/TowerDefenseGame.java
+++ src/towerdefense/game/TowerDefenseGame.java
@@ -30,171 +30,171 @@ import javafx.scene.shape.Rectangle;
  */
 public class TowerDefenseGame {
 
-    /**
-     * The internal calculation-oriented width of each {@link Tile} object.
-     */
-    public static final int TILE_SIZE = 1000;
-    /**
-     * The number of pixels in the visual representation of each {@link Tile}
-     * object.
-     */
-    public static final int TILE_PIXEL_SIZE = 100;
-    /**
-     * The maximum number of movement increments at which a collision is
-     * detected.
-     */
-    public static final int COLLISION_THRESHOLD = 50;
-    private static final int NUM_ROWS = 3;
-    private static final int NUM_TILES_PER_ROW = 8;
-    private final Difficulty difficulty;
-    private Tower selectedTower = null;
-    private final ArrayList<Tower> selectableTowers = new ArrayList();
-    private final Board gameBoard = new Board(NUM_ROWS, NUM_TILES_PER_ROW);
-    private MoneyHandler moneyHandler;
-    private SurvivalTimer survivalTimer;
-    private int playerScore = 0;
+	/**
+	 * The internal calculation-oriented width of each {@link Tile} object.
+	 */
+	public static final int TILE_SIZE = 1000;
+	/**
+	 * The number of pixels in the visual representation of each {@link Tile}
+	 * object.
+	 */
+	public static final int TILE_PIXEL_SIZE = 100;
+	/**
+	 * The maximum number of movement increments at which a collision is
+	 * detected.
+	 */
+	public static final int COLLISION_THRESHOLD = 50;
+	private static final int NUM_ROWS = 3;
+	private static final int NUM_TILES_PER_ROW = 8;
+	private final Difficulty difficulty;
+	private Tower selectedTower = null;
+	private final ArrayList<Tower> selectableTowers = new ArrayList();
+	private final Board gameBoard = new Board(NUM_ROWS, NUM_TILES_PER_ROW);
+	private MoneyHandler moneyHandler;
+	private SurvivalTimer survivalTimer;
+	private int playerScore = 0;
 
-    /**
-     * Constructs a game with the given difficulty.
-     *
-     * @param difficulty the difficulty of the new game
-     */
-    public TowerDefenseGame(Difficulty difficulty) {
-        this.difficulty = difficulty;
-        this.moneyHandler = new MoneyHandler();
-        this.survivalTimer = new SurvivalTimer();
-        selectableTowers.add(new Tower(new Projectile(10, 50,
-                                                      () -> new Circle(10,
-                                                                       Color.BLACK)
-        ),
-                                       60, 500, 100,
-                                       () -> new Rectangle(10, 50,
-                                                           Color.BLACK)
-        ));
-        selectableTowers.add(new Tower(new Projectile(20, 25,
-                                                      () -> new Circle(20,
-                                                                       Color.BLACK)
-        ),
-                                       120, 1000, 100,
-                                       () -> new Rectangle(20, 50,
-                                                           Color.BLACK)
-        ));
-        selectableTowers.add(new Tower(new Projectile(5, 100,
-                                                      () -> new Circle(5,
-                                                                       Color.BLACK)
-        ),
-                                       30, 250, 100,
-                                       () -> new Rectangle(10, 50,
-                                                           Color.BLACK)
-        ));
-    }
+	/**
+	 * Constructs a game with the given difficulty.
+	 *
+	 * @param difficulty the difficulty of the new game
+	 */
+	public TowerDefenseGame(Difficulty difficulty) {
+		this.difficulty = difficulty;
+		this.moneyHandler = new MoneyHandler();
+		this.survivalTimer = new SurvivalTimer();
+		selectableTowers.add(new Tower(new Projectile(10, 50,
+													  () -> new Circle(10,
+																	   Color.BLACK)
+		),
+									   60, 500, 100,
+									   () -> new Rectangle(10, 50,
+														   Color.BLACK)
+		));
+		selectableTowers.add(new Tower(new Projectile(20, 25,
+													  () -> new Circle(20,
+																	   Color.BLACK)
+		),
+									   120, 1000, 100,
+									   () -> new Rectangle(20, 50,
+														   Color.BLACK)
+		));
+		selectableTowers.add(new Tower(new Projectile(5, 100,
+													  () -> new Circle(5,
+																	   Color.BLACK)
+		),
+									   30, 250, 100,
+									   () -> new Rectangle(10, 50,
+														   Color.BLACK)
+		));
+	}
 
-    /**
-     * Checks if the player has sufficient funds to buy the tower; then,
-     * assuming the player has sufficient funds, assigns the passed
-     * {@link Tower} object to the {@link Tile} object at the specified index in
-     * the specified {@link TileRow} object if it is not already associated with
-     * a {@link Tower} object.
-     *
-     * @param rowIndex the index of the {@link TileRow} object within which the
-     * {@link Tower} object should be added
-     * @param tileIndex the index of the {@link Tile} object to which the
-     * {@link Tower} object should be added
-     * @return true if the player has sufficient funds to buy the tower and the
-     * specified {@link Tile} object did not already have a tower on it (i.e.
-     * the tower was added); false otherwise
-     */
-    public boolean tryBuyTower(int rowIndex, int tileIndex) {
-        if (this.selectedTower != null) {
-            Tower towerToBuy = new Tower(this.selectedTower);
-            if (canBuyTower(towerToBuy) && gameBoard.tryAddTowerAt(
-                towerToBuy,
-                rowIndex,
-                tileIndex)) {
-                moneyHandler.purchaseTower(towerToBuy);
-                return true;
-            }
-        }
-        return false;
-    }
+	/**
+	 * Checks if the player has sufficient funds to buy the tower; then,
+	 * assuming the player has sufficient funds, assigns the passed
+	 * {@link Tower} object to the {@link Tile} object at the specified index in
+	 * the specified {@link TileRow} object if it is not already associated with
+	 * a {@link Tower} object.
+	 *
+	 * @param rowIndex the index of the {@link TileRow} object within which the
+	 * {@link Tower} object should be added
+	 * @param tileIndex the index of the {@link Tile} object to which the
+	 * {@link Tower} object should be added
+	 * @return true if the player has sufficient funds to buy the tower and the
+	 * specified {@link Tile} object did not already have a tower on it (i.e.
+	 * the tower was added); false otherwise
+	 */
+	public boolean tryBuyTower(int rowIndex, int tileIndex) {
+		if (this.selectedTower != null) {
+			Tower towerToBuy = new Tower(this.selectedTower);
+			if (canBuyTower(towerToBuy) && gameBoard.tryAddTowerAt(
+					towerToBuy,
+					rowIndex,
+					tileIndex)) {
+				moneyHandler.purchaseTower(towerToBuy);
+				return true;
+			}
+		}
+		return false;
+	}
 
-    /**
-     * Runs one game tick over the {@link Board} object associated with the
-     * game.
-     */
-    public void update() {
-        Tile.clearKilledEnemies();
-        gameBoard.update();
-        updateMoney(Tile.getKilledEnemies(), null);
-        updateTimer();
-    }
+	/**
+	 * Runs one game tick over the {@link Board} object associated with the
+	 * game.
+	 */
+	public void update() {
+		Tile.clearKilledEnemies();
+		gameBoard.update();
+		updateMoney(Tile.getKilledEnemies(), null);
+		updateTimer();
+	}
 
-    public void spawnEnemyAt(int rowIndex) {
-        gameBoard.spawnEnemyAtRow(new Enemy(1, 10, 30, 10,
-                                            () -> new Rectangle(10, 30,
-                                                                Color.RED)),
-                                  rowIndex
-        );
-    }
+	public void spawnEnemyAt(int rowIndex) {
+		gameBoard.spawnEnemyAtRow(new Enemy(1, 10, 30, 10,
+											() -> new Rectangle(10, 30,
+																Color.RED)),
+								  rowIndex
+		);
+	}
 
-    /**
-     * Returns a Node that will allow the entire game to be drawn in one step.
-     *
-     * @return a Node that is a parent of all UI elements of the game board
-     */
-    public Node getDrawableNode() {
-        VBox boardNode = (VBox) gameBoard.getDrawableNode();
-        for (int i = 0; i < NUM_ROWS; i++) {
-            final int row = i;
-            HBox rowNode = (HBox) boardNode.getChildren().get(i);
-            for (int j = 0; j < NUM_TILES_PER_ROW; j++) {
-                final int col = j;
-                rowNode.getChildren().get(j).setOnMousePressed(
-                    (MouseEvent event) -> {
-                        tryBuyTower(row, col);
-                    });
-            }
-        }
-        return boardNode;
-    }
+	/**
+	 * Returns a Node that will allow the entire game to be drawn in one step.
+	 *
+	 * @return a Node that is a parent of all UI elements of the game board
+	 */
+	public Node getDrawableNode() {
+		VBox boardNode = (VBox) gameBoard.getDrawableNode();
+		for (int i = 0; i < NUM_ROWS; i++) {
+			final int row = i;
+			HBox rowNode = (HBox) boardNode.getChildren().get(i);
+			for (int j = 0; j < NUM_TILES_PER_ROW; j++) {
+				final int col = j;
+				rowNode.getChildren().get(j).setOnMousePressed(
+						(MouseEvent event) -> {
+							tryBuyTower(row, col);
+						});
+			}
+		}
+		return boardNode;
+	}
 
-    private boolean canBuyTower(Tower towerToBuy) {
-        return moneyHandler.canBuyTower(towerToBuy);
-    }
+	private boolean canBuyTower(Tower towerToBuy) {
+		return moneyHandler.canBuyTower(towerToBuy);
+	}
 
-    /**
-     * Spawns enemies based on the current state of the game.
-     */
-    private void spawnEnemies() {
-        //if enemy has reached end or a certain amount of time has passed spawn another enenemy
-        //certain enemies depending on certain levels?
-        //time??
-        // TODO create and implement algorithm for spawning enemies based on list of enemies provided (add spawning frequency as attribute? relate to difficulty? need to think about before implementation
-    }
+	/**
+	 * Spawns enemies based on the current state of the game.
+	 */
+	private void spawnEnemies() {
+		//if enemy has reached end or a certain amount of time has passed spawn another enenemy
+		//certain enemies depending on certain levels?
+		//time??
+		// TODO create and implement algorithm for spawning enemies based on list of enemies provided (add spawning frequency as attribute? relate to difficulty? need to think about before implementation
+	}
 
-    public void updateMoney(ArrayList<Enemy> enemiesKilled, Tower towerPurchased) {
-        this.moneyHandler.update(enemiesKilled, towerPurchased);
-    }
+	public void updateMoney(ArrayList<Enemy> enemiesKilled, Tower towerPurchased) {
+		this.moneyHandler.update(enemiesKilled, towerPurchased);
+	}
 
-    public void updateTimer() {
-        this.survivalTimer.update();
-    }
+	public void updateTimer() {
+		this.survivalTimer.update();
+	}
 
-    public MoneyHandler getMoneyHandler() {
-        return this.moneyHandler;
-    }
+	public MoneyHandler getMoneyHandler() {
+		return this.moneyHandler;
+	}
 
-    public SurvivalTimer getSurvivalTimer() {
-        return this.survivalTimer;
-    }
+	public SurvivalTimer getSurvivalTimer() {
+		return this.survivalTimer;
+	}
 
-    public ArrayList<Tower> getSelectableTowers() {
-        return selectableTowers;
-    }
+	public ArrayList<Tower> getSelectableTowers() {
+		return selectableTowers;
+	}
 
-    public void selectTower(Tower towerToSelect) {
-        if (this.selectableTowers.contains(towerToSelect)) {
-            this.selectedTower = towerToSelect;
-        }
-    }
+	public void selectTower(Tower towerToSelect) {
+		if (this.selectableTowers.contains(towerToSelect)) {
+			this.selectedTower = towerToSelect;
+		}
+	}
 }
